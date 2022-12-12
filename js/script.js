fe5b9f473b1bf13a440eeb04a41520fbe924e85c
Kacper Lechicki <klechicki@poczta.fm>
2022-12-12 17:18:59 +0100

    email check with regex

--- js/script.js
+++ js/script.js
@@ -58,6 +58,17 @@ const checkPasswrds = (pass1, pass2) => {
 	}
 };
 
+const checkMail = (email) => {
+	const regex =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	if (regex.test(email.value)) {
+		clearError(email);
+	} else {
+		showError(email, 'E-mail jest niepoprawny!');
+	}
+};
+
 //------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------
@@ -80,4 +91,5 @@ sendBtn.addEventListener('click', (e) => {
 	checkLength(userInput, 5);
 	checkLength(passInput, 8);
 	checkPasswrds(passInput, rpassInput);
+	checkMail(emailInput);
 });
