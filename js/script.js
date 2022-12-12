a45027b9618e7dfcd34bb7cabb57ea59a50883b5
Kacper Lechicki <klechicki@poczta.fm>
2022-12-12 11:25:24 +0100

    variables js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const userInput = document.querySelector('#username');
+const passInput = document.querySelector('#password');
+const rpassInput = document.querySelector('#r_password');
+const emailInput = document.querySelector('#email');
+
+const clearBtn = document.querySelector('.clear');
+const senBtn = document.querySelector('.send');
+
+const popup = document.querySelector('.popup');
+const popupBtn = document.querySelector('.popup-close');
